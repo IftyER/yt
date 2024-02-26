84b2ee802d32aa46fab003014c881ff2e9ae4570
Ifty <72305508+IftyER@users.noreply.github.com>
2024-02-26 14:11:56 +0600

    Update atnnews.py

--- scripts/atnnews.py
+++ scripts/atnnews.py
@@ -14,13 +14,13 @@ def grab(url):
         response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                print('https://github.com/ZazerConer/YouTube-to-M3U8/raw/main/assets/info.m3u8')
+                print('https://github.com/IftyER/yt/raw/main/assets/info.m3u8')
                 return
             #os.system(f'wget {url} -O temp.txt')
             os.system(f'curl "{url}" > temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                print('https://github.com/ZazerConer/YouTube-to-M3U8/raw/main/assets/info.m3u8')
+                print('https://github.com/IftyER/yt/raw/main/assets/info.m3u8')
                 return
     end = response.find('.m3u8') + 5
     tuner = 100
