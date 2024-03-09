6514d9dd84907df705a9d2f23639729054bd8393
Ifty <72305508+IftyER@users.noreply.github.com>
2024-03-09 22:10:45 +0600

    Update 8k.py

--- scripts/8k.py
+++ scripts/8k.py
@@ -14,13 +14,13 @@ def grab(url):
         response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                print('https://github.com/IftyER/yt/raw/main/assets/info.m3u8')
+                print('https://video.twimg.com/ext_tw_video/1766489101535260672/pu/pl/JQ6Dqe7CakAPOolN.m3u8')
                 return
             #os.system(f'wget {url} -O temp.txt')
             os.system(f'curl "{url}" > temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                print('https://github.com/IftyER/yt/raw/main/assets/info.m3u8')
+                print('https://video.twimg.com/ext_tw_video/1766489101535260672/pu/pl/JQ6Dqe7CakAPOolN.m3u8')
                 return
     end = response.find('.m3u8') + 5
     tuner = 100
